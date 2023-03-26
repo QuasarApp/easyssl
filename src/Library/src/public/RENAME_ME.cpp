00a9d0639e91588a709ecad1a923617691636bdd
Andrei Yankovich <EndrIIMail@gmail.com>
2023-03-26 19:36:35 +0300

    Initial commit

---
+++ src/Library/src/public/RENAME_ME.cpp
@@ -0,0 +1,23 @@
+//#
+//# Copyright (C) 2021-2023 QuasarApp.
+//# Distributed under the GPLv3 software license, see the accompanying
+//# Everyone is permitted to copy and distribute verbatim copies
+//# of this license document, but changing it is not allowed.
+//#
+
+#include "RENAME_ME.h"
+
+
+namespace RENAME_ME {
+
+bool init() {
+    initRENAME_MEResources();
+    return true;
+}
+
+QString version() {
+    return RENAME_ME_VERSION;
+}
+
+
+}
